192da9810e4200dc7154bf1baad8d8b831af0e8d
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-08-27 23:22:34 +0530

    Add files via upload

--- windows.py
+++ windows.py
@@ -351,8 +351,9 @@ def welcome_window():
         if result == True:
             welcome.destroy()
     Label(welcome, text = "Welcome to DataSense", font = "Arial 40 bold",bg = "#0d1133", fg = "white").pack(pady=50)
-    Button(welcome, text = "Create Account",font = "Arial 40 bold", command=switchs).pack(pady=10)
-    Button(welcome, text = "Login",font = "Arial 40 bold", command=switchl).pack(pady=10)
+    Button(welcome, text = "Create Account",font = "Arial 30 bold", command=switchs).pack(side = LEFT, padx = 210, pady=60, anchor="sw")
+    Label(welcome, text = "or", font = "Arial 40 italic",bg = "#0d1133", fg = "white").pack(side= BOTTOM, pady=70, anchor="sw")
+    Button(welcome, text = "Login",font = "Arial 30 bold", command=switchl).pack(side = LEFT, padx = 50, pady=60, anchor="sw")
     
 def numeric_window():
     numeric = Tk()
